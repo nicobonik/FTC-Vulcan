aa9bba1bf8c0bf578e30b3c36014c172339861c4
DreekFire <deathtospam314@gmail.com>
2018-10-19 22:16:55 -0700

    added arm auto swinging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeLift.java
@@ -8,15 +8,23 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class IntakeLift {
     private final double closePos = 1.0; //placeholders
     private final double openPos = 0.0;
+    private final double baseArmLength = 16;
+    private final double armAngleOffset = 5;
+    private final double armHeight = 6;
+    private final double omniRadius = 2;
+    private final double extensionRadius = 1;
+    private final double ticksPerRevolution = 1440;
     private int position = 0;
     private DcMotor[] arm;
     private DcMotor intake;
+    private DcMotor extender;
     private CRServo intake1;
     private CRServo intake2;
     private Servo hook;
     private Servo door;
-    public IntakeLift(DcMotor[] arm, DcMotor intake, Servo door) {
+    public IntakeLift(DcMotor[] arm, DcMotor extender, DcMotor intake, Servo door) {
         this.arm = arm;
+        this.extender = extender;
         this.intake = intake;
         //this.hook = hook;
         this.door = door;
@@ -55,9 +63,27 @@ public class IntakeLift {
     }
 
     public void swing(double power) {
+        int pos = Math.max(position + (int)((power / 0.7) * (0.3 * Math.pow(power, 6) + 0.4) * 10), (int)(Math.atan((armHeight - omniRadius) / (baseArmLength + ((extender.getCurrentPosition() * 2 * Math.PI * extensionRadius) / ticksPerRevolution))) * ticksPerRevolution / 360));
+        pos = Math.min(pos, 0);
         for (int i = 0; i < 3; i++) {
-            arm[i].setTargetPosition(position + (int)((power / 0.7) * (0.3 * Math.pow(power, 6) + 0.4) * 10));
+            arm[i].setTargetPosition(pos);
             arm[i].setPower(1.0);
+        }
+    }
+
+    public void swing(boolean up) {
+        if (up) {
+            for (int i = 0; i < 3; i++) {
+                arm[i].setTargetPosition(5);
+                extender.setTargetPosition(0);
+            }
+        } else {
+            for (int i = 0; i < 3; i++) {
+                if (extender.getCurrentPosition() < 100) { // placeholder
+                    arm[i].setTargetPosition(400); // placeholder
+                }
+                arm[i].setTargetPosition((int) (Math.atan((armHeight - omniRadius) / (baseArmLength + ((extender.getCurrentPosition() * 2 * Math.PI * extensionRadius) / ticksPerRevolution))) * ticksPerRevolution / 360));
+            }
         }
     }
 
