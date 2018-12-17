f933932c6e5c44fd47268f692d5e76b34d5da349
DreekFire <deathtospam314@gmail.com>
2018-12-16 19:40:53 -0800

    demo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TurnAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TurnAuto.java
@@ -2,5 +2,16 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
+
 public class TurnAuto extends LinearOpMode {
+    private Robot robot;
+    public void runOpMode() {
+        robot = new Robot(hardwareMap);
+        robot.drivetrain.setupIMU();
+        waitForStart();
+        robot.drivetrain.turn(90);
+        robot.drivetrain.driveForwards(500);
+        robot.stop();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -2,30 +2,31 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
-import java.util.LinkedHashMap;
-
 public class Drivetrain {
-    private DcMotor[] motors = new DcMotor[4];
-    private BNO055IMU imu; //change back to private
+    private DcMotor fl, fr, bl, br;
+    private BNO055IMU imu;
     private BNO055IMU.Parameters parameters;
 
     public Drivetrain(DcMotor frontLeft, DcMotor frontRight, DcMotor backLeft, DcMotor backRight, BNO055IMU IMU) {
-        motors[0] = frontLeft;
-        motors[1] = frontRight;
-        motors[2] = backLeft;
-        motors[3] = backRight;
+        fl = frontLeft;
+        fr = frontRight;
+        bl = backLeft;
+        br = backRight;
         imu = IMU;
 
-        motors[0].setDirection(DcMotor.Direction.FORWARD);
-        motors[1].setDirection(DcMotor.Direction.REVERSE);
-        motors[2].setDirection(DcMotor.Direction.FORWARD);
-        motors[3].setDirection(DcMotor.Direction.REVERSE);
+        fl.setDirection(DcMotor.Direction.FORWARD);
+        fr.setDirection(DcMotor.Direction.REVERSE);
+        bl.setDirection(DcMotor.Direction.FORWARD);
+        br.setDirection(DcMotor.Direction.REVERSE);
+
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void setupIMU() {
@@ -51,13 +52,44 @@ public class Drivetrain {
                 vd * Math.sin(theta) - turn
         };
         for (int i = 0; i < 4; i++) {
-            motors[i].setPower(multiplier * v[i]);
+            fl.setPower(multiplier * v[0]);
+            bl.setPower(multiplier * v[1]);
+            fr.setPower(multiplier * v[2]);
+            br.setPower(multiplier * v[3]);
         }
     }
 
+    public void driveForwards(int ticks) {
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fl.setTargetPosition(ticks);
+        bl.setTargetPosition(ticks);
+        fr.setTargetPosition(-ticks);
+        br.setTargetPosition(-ticks);
+        while(fl.isBusy() || fr.isBusy() || bl.isBusy() || br.isBusy()) {}
+    }
+
+    public void turn(double degrees) {
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        double error;
+        do {
+            error = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - degrees;
+            fl.setPower(error * 0.025);
+            bl.setPower(error * 0.025);
+            fr.setPower(-error * 0.025);
+            br.setPower(-error * 0.025);
+        } while(Math.abs(error) > 3);
+    }
+
     public void stop() {
-        for(int i = 0; i < 4; i++) {
-            motors[i].setPower(0);
-        }
     }
 }
